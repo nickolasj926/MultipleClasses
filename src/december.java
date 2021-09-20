3ba60c76e8b0c81ec2937e9337f415f565673490
Nick Johnson <89210926+nickolasj926@users.noreply.github.com>
2021-09-20 12:19:37 -0500

    Update december.java

--- src/december.java
+++ src/december.java
@@ -1,4 +1,4 @@
-public class december {
+public class December {
     public void simpleMessage(String name) {
         System.out.println("Nice, I've heard that extraordinary people are born in " + name + "!");
 
